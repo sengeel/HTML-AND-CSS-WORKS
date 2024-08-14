484a31584340038020687aa8ebe5205c4e087170
Chizurum Okere-Olujie <okereolujiechizurum@gmail.com>
2024-08-14 15:18:26 +0100

    ALL MY HTML AND CSS

---
+++ Day 11 HTML Class/chizurum.js
@@ -0,0 +1,6 @@
+const toggleham=document.getElementById("toggle-ham");
+
+const togglemenu=document.getElementById("toggle-menu");
+
+toggleham.addEventListener('click', ()=> { togglemenu.classList.toggle("active");
+})
